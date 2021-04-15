5d2ac51f181b13580430f3ab5d55d67fcf670e67
Alessandro Gerelli <geroale2000@gmail.com>
2021-04-16 01:57:36 +0200

    Fixes

--- android/src/main/java/com/reactnativemediastore/MediastoreModule.kt
+++ android/src/main/java/com/reactnativemediastore/MediastoreModule.kt
@@ -15,30 +15,30 @@ class MediastoreModule(reactContext: ReactApplicationContext) : ReactContextBase
         return "Mediastore"
     }
 
-    @ReactMethod
-    fun readAudioVideoExternalMedias(promise: Promise) {
+    private fun mapFiles(
+      collection: Uri,
+      externalContentUri: Uri,
+      idColumn: String,
+      nameColumn: String,
+      durationColumn: String,
+      sizeColumn: String,
+      mimeColumn: String,
+      titleColumn: String,
+      albumColumn: String,
+      artistColumn: String
+    ): Array<WritableMap> {
 
-      data class Media(val uri: Uri,
-                       val name: String,
-                       val duration: Int,
-                       val size: Int
-      )
-      val mediaList = Arguments.createArray()
-
-      val collection =
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-          MediaStore.Audio.Media.getContentUri(
-            MediaStore.VOLUME_EXTERNAL
-          )
-        } else {
-          MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-        }
+      val files = mutableListOf<WritableMap>()
 
       val projection = arrayOf(
-        MediaStore.Audio.Media._ID,
-        MediaStore.Audio.Media.DISPLAY_NAME,
-        MediaStore.Audio.Media.DURATION,
-        MediaStore.Audio.Media.SIZE
+        idColumn,
+        nameColumn,
+        durationColumn,
+        sizeColumn,
+        mimeColumn,
+        titleColumn,
+        albumColumn,
+        artistColumn
       )
 
       val query = reactApplicationContext.contentResolver.query(
@@ -48,14 +48,15 @@ class MediastoreModule(reactContext: ReactApplicationContext) : ReactContextBase
         null,
         null
       )
-      val use = query?.use { cursor ->
-        // Cache column indices.
-        val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
-        val nameColumn =
-          cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)
-        val durationColumn =
-          cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
-        val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE)
+      query?.use { cursor ->
+        val idColumn = cursor.getColumnIndexOrThrow(idColumn)
+        val nameColumn = cursor.getColumnIndexOrThrow(nameColumn)
+        val durationColumn = cursor.getColumnIndexOrThrow(durationColumn)
+        val sizeColumn = cursor.getColumnIndexOrThrow(sizeColumn)
+        val mimeColumn = cursor.getColumnIndexOrThrow(mimeColumn)
+        val titleColumn = cursor.getColumnIndexOrThrow(titleColumn)
+        val albumColumn = cursor.getColumnIndexOrThrow(albumColumn)
+        val artistColumn = cursor.getColumnIndexOrThrow(artistColumn)
 
         while (cursor.moveToNext()) {
 
@@ -66,14 +67,67 @@ class MediastoreModule(reactContext: ReactApplicationContext) : ReactContextBase
           item.putString("name", cursor.getString(nameColumn))
           item.putInt("duration", cursor.getInt(durationColumn))
           item.putInt("size", cursor.getInt(sizeColumn))
+          item.putString("mime", cursor.getString(mimeColumn))
+          item.putString("title", cursor.getString(titleColumn))
+          item.putString("album", cursor.getString(albumColumn))
+          item.putString("artist", cursor.getString(artistColumn))
 
-          item.putString("contentUri", ContentUris.withAppendedId(
-            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
-            id
-          ).path)
+          item.putString("contentUri", "content://media" + externalContentUri.path + "/" + id)
 
-          mediaList.pushMap(item)
+          files += item
         }
+      }
+
+      return files.toTypedArray()
+    }
+
+    @ReactMethod
+    fun readAudioVideoExternalMedias(promise: Promise) {
+
+      data class Media(
+        val uri: Uri,
+        val name: String,
+        val duration: Int,
+        val size: Int
+      )
+      val mediaList = Arguments.createArray()
+
+      mapFiles(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+          MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+        } else {
+          MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        },
+        MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+        MediaStore.Audio.Media._ID,
+        MediaStore.Audio.Media.DISPLAY_NAME,
+        MediaStore.Audio.Media.DURATION,
+        MediaStore.Audio.Media.SIZE,
+        MediaStore.Audio.Media.MIME_TYPE,
+        MediaStore.Audio.Media.TITLE,
+        MediaStore.Audio.Media.ALBUM,
+        MediaStore.Audio.Media.ARTIST
+      ).forEach { file ->
+        mediaList.pushMap(file)
+      }
+
+      mapFiles(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+          MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+        } else {
+          MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+        },
+        MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+        MediaStore.Video.Media._ID,
+        MediaStore.Video.Media.DISPLAY_NAME,
+        MediaStore.Video.Media.DURATION,
+        MediaStore.Video.Media.SIZE,
+        MediaStore.Video.Media.MIME_TYPE,
+        MediaStore.Video.Media.TITLE,
+        MediaStore.Video.Media.ALBUM,
+        MediaStore.Video.Media.ARTIST
+      ).forEach { file ->
+        mediaList.pushMap(file)
       }
 
       promise.resolve(mediaList)
